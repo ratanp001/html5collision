ba17cfce702c56c423a4a1253bb4a32da3b4b91e
Ratan Parmar <ratanp@google.com>
2019-03-04 16:28:51 +0530

    new commit

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -96,11 +96,7 @@
 "use strict";
 
 
-var _utils = __webpack_require__(/*! ./utils */ "./src/utils.js");
-
-var _utils2 = _interopRequireDefault(_utils);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _utils = __webpack_require__(/*! ./utils.js */ "./src/utils.js");
 
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
@@ -128,26 +124,6 @@ addEventListener('resize', function () {
     init();
 });
 
-// Objects
-function Circle(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-}
-
-Circle.prototype.draw = function () {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
-    c.closePath();
-};
-
-Circle.prototype.update = function () {
-    this.draw();
-};
-
 function getDistance(x1, y1, x2, y2) {
     var xDistance = x2 - x1;
     var yDistance = y2 - y1;
@@ -155,38 +131,81 @@ function getDistance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
 
+// Objects
+function Circle(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.velocity = {
+        x: Math.random() - 0.5,
+        y: Math.random() - 0.5
+    };
+    this.radius = radius;
+    this.color = color;
+}
+
+Circle.prototype.update = function (circleArr) {
+    console.log(circleArr);
+    console.log(undefined);
+    undefined.draw();
+
+    for (var i = 0; i < circleArr.length; i++) {
+        console.log(i + circleArr[i].y);
+
+        if (undefined === circleArr[i]) continue;
+        if (getDistance(undefined.x, undefined.y, circleArr[i].x, circleArr[i].y) - undefined.radius * 2 < 0) {
+            console.log("particles has collided!!");
+        }
+    }
+    undefined.x += undefined.velocity.x;
+    undefined.y += undefined.velocity.y;
+};
+
+Circle.prototype.draw = function () {
+    c.beginPath();
+    c.arc(undefined.x, undefined.y, undefined.radius, 0, Math.PI * 2, false);
+    //c.fillStyle = this.color
+    c.strokeStyle = undefined.color;
+    c.stroke();
+    c.closePath();
+};
+
 // Implementation
-var circle1 = void 0;
-var circle2 = void 0;
-
+var circles = void 0;
+//let circle2;
+var circleArr = void 0;
 function init() {
-    circle1 = new Circle(800, 500, 100, 'black');
-    circle2 = new Circle(undefined, undefined, 40, 'red');
-    //objects = []
 
-    /*for (let i = 0; i < 400; i++) {
-        // objects.push()
-    }*/
+    circleArr = [];
+    for (var i = 0; i <= 4; i++) {
+        var radius = 100;
+        var x = (0, _utils.randomIntFromRange)(radius, canvas.width - radius);
+        var y = (0, _utils.randomIntFromRange)(radius, canvas.height - radius);
+
+        if (i !== 0) {
+            // console.log("hi");
+            for (var j = 0; j < circleArr.length; j++) {
+
+                if (getDistance(x, y, circleArr[j].x, circleArr[j].y) - radius * 2 < 0) {
+
+                    x = (0, _utils.randomIntFromRange)(radius, canvas.width - radius);
+                    y = (0, _utils.randomIntFromRange)(radius, canvas.height - radius);
+                    j = -1;
+                }
+            }
+        }
+        circleArr.push(new Circle(x, y, radius, 'black'));
+    }
+    // console.log(circleArr);
 }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
-
-    //c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-    // objects.forEach(object => {
-    circle1.update();
-    circle2.x = mouse.x;
-    circle2.y = mouse.y;
-    circle2.update();
-    if (getDistance(circle1.x, circle1.y, circle2.x, circle2.y) < circle1.radius + circle2.radius) {
-        circle1.color = 'red';
-        console.log("hi");
-    } else {
-        circle1.color = 'black';
-    }
-    // })
+    circleArr.forEach(function (circle) {
+        console.log("before :" + circle.x);
+        circle.update(circleArr);
+    });
 }
 
 init();
--- src/canvas.js
+++ src/canvas.js
@@ -1,4 +1,4 @@
-import utils from './utils'
+import {randomIntFromRange,randomColor} from './utils.js'
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
@@ -26,26 +26,6 @@ addEventListener('resize', () => {
     init()
 })
 
-// Objects
-function Circle(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-}
-
-Circle.prototype.draw = function() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-}
-
-Circle.prototype.update = function() {
-    this.draw()
-}
-
 function getDistance(x1,y1,x2,y2){
     var xDistance = x2 - x1;
     var yDistance = y2 - y1;
@@ -53,38 +33,88 @@ function getDistance(x1,y1,x2,y2){
     return Math.sqrt(Math.pow(xDistance,2)+ Math.pow(yDistance,2));
 }
 
+// Objects
+function Circle(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.velocity={
+        x: Math.random() -0.5,
+        y: Math.random() - 0.5
+    }
+    this.radius = radius;
+    this.color = color;
+}
+
+
+Circle.prototype.update = circleArr => {
+    console.log(circleArr);
+    console.log(this);
+    this.draw();
+
+    for (let i = 0; i < circleArr.length; i++) {
+        console.log(i+circleArr[i].y);
+        
+       if(this === circleArr[i]) continue;
+            if(getDistance(this.x,this.y,circleArr[i].x,circleArr[i].y) - (this.radius* 2 ) < 0){
+                console.log("particles has collided!!");
+            }
+    }
+    this.x +=this.velocity.x;
+    this.y +=this.velocity.y;
+    
+}
+
+Circle.prototype.draw = () => {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    //c.fillStyle = this.color
+    c.strokeStyle = this.color;
+    c.stroke();
+    c.closePath()
+}
+
+
+
 // Implementation
-let circle1;
-let circle2;
-
+let circles;
+//let circle2;
+let circleArr;
 function init() {
-    circle1 = new Circle(800,500,100,'black');
-    circle2 = new Circle(undefined,undefined,40,'red');
-    //objects = []
+    
+    circleArr = [];
+    for (var i = 0; i <= 4; i++) {
+        let radius = 100;
+        let x = randomIntFromRange(radius,canvas.width - radius);
+        let y = randomIntFromRange(radius,canvas.height - radius);
+        
+        if(i!==0){
+           // console.log("hi");
+            for (let j = 0; j < circleArr.length; j++) {
+               
+                if(getDistance(x,y,circleArr[j].x,circleArr[j].y) - (radius* 2 ) < 0){
 
-    /*for (let i = 0; i < 400; i++) {
-        // objects.push()
-    }*/
+                    
+                     x = randomIntFromRange(radius,canvas.width - radius);
+                    y = randomIntFromRange(radius,canvas.height - radius);
+                     j = -1;   
+                }
+            }
+        }
+    circleArr.push(new Circle(x,y,radius,'black'));
+
+    }
+   // console.log(circleArr);
+
 }
 
 // Animation Loop
 function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-
-    //c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-    // objects.forEach(object => {
-      circle1.update();
-      circle2.x = mouse.x;
-      circle2.y = mouse.y;
-      circle2.update();
-      if(getDistance(circle1.x,circle1.y,circle2.x,circle2.y) < circle1.radius+circle2.radius){
-        circle1.color = 'red';
-      console.log("hi");
-  }else {
-    circle1.color = 'black';
-  }
-    // })
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    circleArr.forEach(circle=>{
+       console.log("before :"+circle.x);
+        circle.update(circleArr);
+    })
 }
 
 init()
